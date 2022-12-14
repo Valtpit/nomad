5325e1cd1223660894f8dca8abceff3dcc9c02fd
Valtpit <77121949+Valtpit@users.noreply.github.com>
2022-12-14 22:01:56 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div id="menu">
       <div className='site-name'>
-        <p>News site</p>
+        <p>News Site</p>
       </div>
       <div id="menu-items">
           <Link className='menu-item' to="/" >Home</Link>
